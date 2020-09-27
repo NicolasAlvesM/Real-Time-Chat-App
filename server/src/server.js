c29d72f77ddfabba0d6e005f6df414a764d32b66
Nicolas Moreno Alves Medeiros <61037425+NicolasMorenoAlves@users.noreply.github.com>
2020-09-27 16:08:29 -0300

    Add files via upload

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,23 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const express = require('express');
+const app=express()
 const cors=require('cors')
 app.use(cors())
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const users=[]
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.emit('connection','ok')
+  console.log('a user connected');
+  socket.on('newUser',(user,roomName)=>{
+    socket.join(roomName)
+    users[socket.id]=user
     socket.on('chat message', (msg) => {
-        io.emit('chat message', {msg,id:socket.id});
-        console.log('message: ' + msg);
+      io.to(roomName).emit('chat message', {name:users[socket.id],msg,id:socket.id});
       });
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
+  })
+  socket.emit('connection','ok')
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    delete users[socket.id]
   });
-
-http.listen(3333, () => {
-  console.log('listening on *:3333');
 });
+http.listen(3333);
